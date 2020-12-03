f8f0a23679844ef86c7bc966a023b55602a3191a
alex <alexroat@gmail.com>
2020-12-03 15:43:03 +0100

    icons on mod records

--- src/table.js
+++ src/table.js
@@ -95,7 +95,7 @@ class RowHeaderCell extends Html.Th
         const {table, row} = this.props;
         this.text(row[""].i)
         if (row[""].op)
-            new Icon({icon:{UPDATE:"asterisk",CREATE:"plus",DELETE:"times"}[row[""].op]}).appendTo(this)
+            new Icon({icon: {UPDATE: "asterisk", CREATE: "plus", DELETE: "times"}[row[""].op]}).appendTo(this)
         return super.doLayout();
     }
 }
@@ -136,7 +136,8 @@ class Cell extends Html.Td
     doLayout()
     {
         const fnf = this.props.col.fmtCell || Cell.fmtCell
-        fnf.call(this);
+        if (!this.el.contains(document.activeElement))
+            fnf.call(this);
         return super.doLayout();
     }
     static fmtCell()
@@ -156,14 +157,16 @@ class Cell extends Html.Td
                 "DATETIME": DateTimeInput,
             })[col.type] || Html.Input;
             const i = new cl().appendTo(this).val(val);
-            i.on("change", () => {
+            i.on("change", (e) => {
+                e.stopPropagation()
                 row[col.name] = i.val();
                 if (row[""].op != "CREATE")
                 {
                     row[""].op = "UPDATE";
                     table.mods[row[""].k] = row;
                 }
-                table.doLayout();
+                self.parent(Row).doLayout();
+                return false;
             });
 
         } else
@@ -173,7 +176,25 @@ class Cell extends Html.Td
 
 class Row extends Html.Tr
 {
-
+    constructor(props)
+    {
+        super(props)
+        const {table}=this.props,self=this;
+        this.on("click", function (e) {
+            e.stopPropagation()
+            table.setCurrentRow(self);
+            return false;
+        })
+    }
+    isCurrent()
+    {
+        return this.props.table.current == this.props.row[""].k;
+    }
+    doLayout()
+    {
+        this.toggleClass("current", this.isCurrent());
+        return super.doLayout()
+    }
 }
 
 export class DataTable extends Table
@@ -187,6 +208,7 @@ export class DataTable extends Table
         this.sorts = [];
         this.filters = [];
         this.mods = {};
+        this.current = null;
         this.setData({cols: [{name: "a"}, {name: "b"}], rows: [{a: 1, b: 2}, {a: 3, b: 4}]});
         this.toolbar = new ToolBar().prependTo(this, {w: 30});
         this.pager = new Pager().appendTo(this.toolbar);
@@ -219,18 +241,19 @@ export class DataTable extends Table
     }
     refresh()
     {
+        const {pageSize, page} = this.props;
         const self = this;
         const table = this;
         this.clear();
-        const trhead = new Row({table}).appendTo(this.head);
+        const trhead = new Row({row: {"": {}}, table}).appendTo(this.head);
         new Html.Th().appendTo(trhead);
         for (var col of this.cols)
             new ColHeaderCell({col, table}).appendTo(trhead);
         const drows = this.rows.concat(this.newRows())
-        var i=0;
+        var i = page * pageSize;
         for (var row of drows)
         {
-            row[""].i=i++;
+            row[""].i = i++;
             if (this.mods[row[""].k])
                 row = this.mods[row[""].k];
             var tr = new Row({row, table}).appendTo(this.body);
@@ -238,7 +261,21 @@ export class DataTable extends Table
             for (var col of this.cols)
                 new Cell({row, col, table}).appendTo(tr);
         }
+        this.setCurrentRow();
         this.doLayout();
+    }
+    setCurrentRow(tr=this.getCurrentRow())
+    {
+        this.current = tr.props.row[""].k;
+        return this.doLayout();
+    }
+    getCurrentRow()
+    {
+        const cc = this.body.children()
+        for (var tr of cc)
+            if (tr.isCurrent())
+                return tr;
+        return cc.length ? cc[0] : null;
     }
     setSort(name, mode, append)
     {
@@ -281,7 +318,14 @@ export class DataTable extends Table
     }
     async deleteRow()
     {
-
+        const trc=this.getCurrentRow()
+        if (!trc)
+            return;
+        const row=trc.props.row;
+        row[""].op="DELETE"
+        this.mods[row[""].k]=row;
+        console.log(row);
+        this.refresh();
     }
     async duplicateRow()
     {
